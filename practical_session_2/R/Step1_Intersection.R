4a3a2b9bf2480dfbd0131e755b2b4f901ff2f7a3
ENA <enrico.e.armelloni@gmail.com>
2022-02-02 16:14:16 +0100

    description

--- practical_session_2/R/Step1_Intersection.R
+++ practical_session_2/R/Step1_Intersection.R
@@ -1,20 +1,38 @@
+# This code is based on the workflow adopted to elaborate Automatic Identification System (AIS) data for the report "Identification and mapping of bottom trawl fishing grounds for deep-water red shrimp in the Eastern-Central Mediterranean Sea (GSAs 12-16, 18-27)", requested by the General Fisheries Commitee for the Mediterranean Sea (GFCM). 
+# ---
+# title: "Intersect AIS trawling fishing tracks to a spatial grid"
+# authors: "Enrico Nicola Armelloni, Jacopo Pulcinella"
+# date: "February 1st, 2022"
+# ---
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/
+# 
+# contact address: maps.irbim@irbim.cnr.it
+
+
+
 # Check Working directory
 getwd() # The WD have to be "practical_session_2"
 setwd('practical_session_2/') # Push to set the WD to a backward place
 getwd() # The WD have to be "practical_session_2"
 
 # Load libraries and functions
-
 source("R/supporting_code/global_functions_light.R")
 library(sf)
 library(lubridate)
 library(tidyverse)
-
-source("R/supporting_code/global_functions_light.R")
-
 wgs="+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
-options(warn=-1)
-# sf::use_sf_s2(FALSE)
+
 # Load base grid ####
 grid=read_sf("maps/grid")
 st_crs(grid)=wgs
@@ -44,7 +62,7 @@ for(j in 1:length(ref_years)){
   f_tracks=split(f_tracks, f_tracks$MMSI)
   
   # Apply function to estimate fishing activity
-  f_tracks_grid=suppressMessages(estimate_fishing_effort(f_tracks, grid))
+  f_tracks_grid=suppressMessages(estimate_fishing_effort_DWRS(f_tracks, grid))
   
   # Format dataset
   f_tracks_grid=plyr::ldply(f_tracks_grid,.id = NULL)
